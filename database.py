b1e4e743e256f9a13848427eab7f1ec41e430979
bellsophie-87 <anabelzuluetaamador@gmail.com>
2024-10-31 00:13:27 +0000

    init

--- database.py
+++ database.py
@@ -3,5 +3,16 @@ from azure.cosmos import CosmosClient, exceptions, PartitionKey
 COSMOS_ENDPOINT = ''
 COSMOS_KEY = ''
 
-DATABASE_NAME = ' '
-CONTAINE_NAME = ' '
+DATABASE_NAME = 'test-db'
+CONTAINE_NAME = 'events'
+
+# Inicializamos cliente de Cosmos
+client = CosmosClient(COSMOS_ENDPOINT, COSMOS_KEY)
+
+# Crea u obtiene la bade de datos
+database = client.create_database_if_not_exists(id = DATABASE_NAME)
+
+# Crea u obtiene el container
+container = database.create_container_if_not_exists(id = CONTAINE_NAME,
+                                                   partition_key = PartitionKey(path = "/id"),
+                                                   offer_throughput = 400)
